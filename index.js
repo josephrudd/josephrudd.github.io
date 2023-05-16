d3cbfa858f1c92b8e764927bdbcba1ad04a0c241
josephrudd <113957328+josephrudd@users.noreply.github.com>
2023-05-16 10:50:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#button').addEventListener('click', function(e) {
+        alert('I said not to, OK?');
+    });
+});
